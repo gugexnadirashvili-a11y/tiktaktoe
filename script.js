3d18f252b4f2589c18f983ed3af9e8932a34a4bb
gugexnadirashvili-a11y <gugexnadirashvili@gmail.com>
2026-06-10 09:33:53 -0700

    Refactor game logic and improve code readability

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // დაფის მნიშვნელობა
-var origBoard;
+let origBoard;
 
 // მოთამაშეები
 const huPlayer = 'O';
@@ -13,35 +13,43 @@ const winCombos = [
     [0, 3, 6], // მარცხენა სვეტი
     [1, 4, 7], // შუა სვეტი
     [2, 5, 8], // მარჯვენა სვეტი
-    [0, 4, 8], // დიაგონალი მარცხნიდან მარჯვნივ
-    [6, 4, 2]  // დიაგონალი მარცხნიდან მარჯვნივ
+    [0, 4, 8], // დიაგონალი
+    [6, 4, 2]  // დიაგონალი
 ];
 
 // HTML-ის უჯრის ელემენტები
 const cells = document.querySelectorAll('.cell');
 
-// ტამაშის დაწყება
+// თამაშის დაწყება
 startGame();
 
 function startGame() {
-    // მალავს წინა შედეგის სედეგს
+    // მალავს წინა შედეგს
     document.querySelector(".endgame").style.display = "none";
-    // ქმნის მასივს 9 ელემენტიით (0-8)
+
+    // ქმნის ცარიელ დაფას (0-8)
     origBoard = Array.from(Array(9).keys());
-    // განვაახლოთ დადა და დავაყენოთ Event listen-ერები ყოველი უჯრისათვის
-    for (var i = 0; i < cells.length; i++) {
+
+    // ვასუფთავებთ დაფას და ვამატებთ click listener-ებს
+    for (let i = 0; i < cells.length; i++) {
         cells[i].innerText = '';
         cells[i].style.removeProperty('background-color');
+
+        // რომ არ დაემატოს ორჯერ
+        cells[i].removeEventListener('click', turnClick, false);
         cells[i].addEventListener('click', turnClick, false);
     }
 }
 
 function turnClick(square) {
-    // თუ დაჭერილი უჯრა ცარიელია შეასრულე შემდეგი
-    if (typeof origBoard[square.target.id] == 'number') {
-        // მოთამაშე ასრულებს მოქმედებას
+
+    // თუ უჯრა თავისუფალია
+    if (typeof origBoard[square.target.id] === 'number') {
+
+        // ადამიანის სვლა
         turn(square.target.id, huPlayer);
-        // თუ თამაში ფრე ან მოგებული არ არის, კომპიუტერი ასრულებს სვლას
+
+        // თუ თამაში არ დასრულდა, კომპიუტერი აკეთებს სვლას
         if (!checkWin(origBoard, huPlayer) && !checkTie()) {
             aiTurn();
         }
@@ -49,129 +57,193 @@ function turnClick(square) {
 }
 
 function turn(squareId, player) {
-    // განვაახლოტ დაფა შესრულებული მოქმედების მიხედვით
+
+    // განვაახლოთ დაფა
     origBoard[squareId] = player;
+
+    // განვაახლოთ HTML
     document.getElementById(squareId).innerText = player;
-    // ვამოწმებთ მორჩა თამაში თუ არა
+
+    // შევამოწმოთ მოგება
     let gameWon = checkWin(origBoard, player);
-    if (gameWon) gameOver(gameWon);
+
+    if (gameWon) {
+        gameOver(gameWon);
+    }
 }
 
 function aiTurn() {
-    while (!checkWin(origBoard, aiPlayer) && !checkTie()) {
+
+    // კომპიუტერი აკეთებს მხოლოდ ერთ სვლას
+    if (!checkWin(origBoard, aiPlayer) && !checkTie()) {
         turn(bestSpot(), aiPlayer);
-        if (checkWin(origBoard, aiPlayer) || checkTie()) {
-            break;
-        }
-        turn(bestSpot(), huPlayer);
     }
 }
 
 function checkWin(board, player) {
-    // შევაგროვოთ ყველა უჯრის კორდინატი რომელიც მოთამაშეს აქვს მონიშნული
-    let plays = board.reduce((a, e, i) => (e === player) ? a.concat(i) : a, []);
+
+    // მოთამაშის დაკავებული უჯრები
+    let plays = board.reduce(
+        (a, e, i) => (e === player ? a.concat(i) : a),
+        []
+    );
+
     let gameWon = null;
-    // ვამოწმებთ მოთამაშის დაკავებული უჯრებიდან რომელიმე თუ ავსებს მომგებიან კომბინაციებს
+
+    // ვამოწმებთ მომგებიან კომბინაციებს
     for (let [index, win] of winCombos.entries()) {
+
         if (win.every(elem => plays.indexOf(elem) > -1)) {
-            gameWon = {index: index, player: player};
+
+            gameWon = {
+                index: index,
+                player: player
+            };
+
             break;
         }
     }
+
     return gameWon;
 }
 
 function gameOver(gameWon) {
-    // მკვეთრად გამოვაჩინოთ მომგებიანი კომბინაცია
+
+    // გამოვაჩინოთ მომგებიანი კომბინაცია
     for (let index of winCombos[gameWon.index]) {
+
         document.getElementById(index).style.backgroundColor =
-            gameWon.player == huPlayer ? "blue" : "red";
+            gameWon.player === huPlayer ? "blue" : "red";
     }
-    //მოვაშოროთ უჯრაზე დაჭერის listen-ერები
-    for (var i = 0; i < cells.length; i++) {
+
+    // მოვაშოროთ click listener-ები
+    for (let i = 0; i < cells.length; i++) {
+
         cells[i].removeEventListener('click', turnClick, false);
     }
-    // გამოვაცხადოთ მოიგო თუ წააგო მოთამაშემ
-    declareWinner(gameWon.player == huPlayer ? "მოიგე!" : "წააგე :(");
+
+    // შედეგის გამოცხადება
+    declareWinner(
+        gameWon.player === huPlayer ? "მოიგე!" : "წააგე :("
+    );
 }
 
 function declareWinner(who) {
+
     document.querySelector(".endgame").style.display = "block";
+
     document.querySelector(".endgame .text").innerText = who;
 }
 
-function emptySquares() {
-    // დავაბრუნოთ ყველა დაკავებული უჯრა
-    return origBoard.filter(s => typeof s == 'number');
+function emptySquares(board) {
+
+    // ვაბრუნებთ თავისუფალ უჯრებს
+    return board.filter(s => typeof s === 'number');
 }
 
 function bestSpot() {
-    // ვიპოვოთ საუკეთესო სვლა მინი მაქს ალგორითმის საშვალებით
+
+    // საუკეთესო სვლა minimax-ით
     return minimax(origBoard, aiPlayer).index;
 }
 
 function checkTie() {
-    // ვამოწმებთ თუ არც მოგებული გვყავს და არც ცარიელი ადგილები ფრეა
-    if (emptySquares().length == 0) {
-        for (var i = 0; i < cells.length; i++) {
+
+    // თუ თავისუფალი უჯრები აღარ დარჩა
+    if (emptySquares(origBoard).length === 0) {
+
+        for (let i = 0; i < cells.length; i++) {
+
             cells[i].style.backgroundColor = "green";
+
             cells[i].removeEventListener('click', turnClick, false);
         }
+
         declareWinner("ფრე!");
+
         return true;
     }
+
     return false;
 }
 
 function minimax(newBoard, player) {
-    // ყველა თავისუფალი უჯრა
-    var emptySpots = emptySquares();
 
-    // შევამოწმოთ თამაშის სიტუაცია
+    // თავისუფალი უჯრები მოცემულ დაფაზე
+    let emptySpots = emptySquares(newBoard);
+
+    // საბოლოო მდგომარეობების შემოწმება
     if (checkWin(newBoard, huPlayer)) {
-        return {score: -10}; // ადამიანმა მოიგო
-    } else if (checkWin(newBoard, aiPlayer)) {
-        return {score: 10}; // კომპიუტერმა მოიგო
-    } else if (emptySpots.length === 0) {
-        return {score: 0}; // ფრე
+        return { score: -10 };
     }
 
-    var moves = [];
-    // შემოვუაროთ თავისუფალ უჯრებს
-    for (var i = 0; i < emptySpots.length; i++) {
-        var move = {};
+    if (checkWin(newBoard, aiPlayer)) {
+        return { score: 10 };
+    }
+
+    if (emptySpots.length === 0) {
+        return { score: 0 };
+    }
+
+    let moves = [];
+
+    // ყველა შესაძლო სვლის გამოცდა
+    for (let i = 0; i < emptySpots.length; i++) {
+
+        let move = {};
+
         move.index = newBoard[emptySpots[i]];
+
+        // დროებით ვასრულებთ სვლას
         newBoard[emptySpots[i]] = player;
 
-        // რეკურსიულად გამოვიძახოთ მინი მაქსის ალგორითმი შემდეგი მოთამაშესთვის
-        if (player == aiPlayer) {
-            var result = minimax(newBoard, huPlayer);
+        // რეკურსიული გამოძახება
+        if (player === aiPlayer) {
+
+            let result = minimax(newBoard, huPlayer);
+
             move.score = result.score;
+
         } else {
-            var result = minimax(newBoard, aiPlayer);
+
+            let result = minimax(newBoard, aiPlayer);
+
             move.score = result.score;
         }
 
-        // განვაახლოთ დათა
+        // ვაბრუნებთ საწყის მდგომარეობას
         newBoard[emptySpots[i]] = move.index;
+
         moves.push(move);
     }
 
-    // ავარჩიოთ საუკეთესო სვლა
-    var bestMove;
-    if(player === aiPlayer) {
-        var bestScore = -10000;
-        for(var i = 0; i < moves.length; i++) {
+    // საუკეთესო სვლის არჩევა
+    let bestMove;
+
+    if (player === aiPlayer) {
+
+        let bestScore = -10000;
+
+        for (let i = 0; i < moves.length; i++) {
+
             if (moves[i].score > bestScore) {
+
                 bestScore = moves[i].score;
+
                 bestMove = i;
             }
         }
+
     } else {
-        var bestScore = 10000;
-        for(var i = 0; i < moves.length; i++) {
+
+        let bestScore = 10000;
+
+        for (let i = 0; i < moves.length; i++) {
+
             if (moves[i].score < bestScore) {
+
                 bestScore = moves[i].score;
+
                 bestMove = i;
             }
         }
